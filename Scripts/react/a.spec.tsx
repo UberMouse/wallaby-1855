1fc9b2bca46f40d0628437c5fb9372a0ea66c50c
Taylor Lodge <ubermouse894@gmail.com>
2018-10-05 16:15:33 +1300

    Add in our custom stuff

--- Scripts/react/a.spec.tsx
+++ Scripts/react/a.spec.tsx
@@ -1,3 +1,23 @@
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+import { createSerializer } from 'enzyme-to-json';
+
+expect.addSnapshotSerializer(createSerializer({
+    map: json => ({
+        ...json,
+        props: json.props
+    })
+}));
+
+Enzyme.configure({
+    adapter: new Adapter()
+});
+
+import React from 'react';
+import { mount } from 'enzyme';
+
+const Test: React.StatelessComponent<{}> = () => <p>Test</p>;
+
 it('works', () => {
-    expect({}).toMatchSnapshot();
+    expect(mount(<Test />)).toMatchSnapshot();
 });
